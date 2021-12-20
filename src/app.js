1f9fe12e5f214e3c705fb04b21d0ffcd3f9f5c91
Fabricio Garcia <fabgrel10@gmail.com>
2021-12-19 22:34:32 -0600

    🎉 Initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import './scss/app.scss';
+
+const app = document.getElementById('app');
+const p = document.createElement('p');
+
+app.innerHTML = '<h1>Webpack 5 Boilerplate</h1>';
+p.textContent = 'Hello World!';
+app.appendChild(p);
